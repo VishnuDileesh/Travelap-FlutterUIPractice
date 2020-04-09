f525880375fd72ec52dba60618114aab301a5084
Pheyonix <dev@vishnudileesh.com>
2020-04-09 14:28:57 +0530

    Add Asset, Add header text section & bg image

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,9 +3,51 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget{
 	@override
 	Widget build(BuildContext context) {
-		return Container(
-			child: Center(
-				child: Text("Travelap"),
+
+		Size size = MediaQuery.of(context).size;
+
+		return Material(
+			child: Stack(
+				children: <Widget>[
+					Center(
+						child: Image.asset(
+							'assets/bg-image.jpg',
+							width: size.width,
+							height: size.height,
+							fit: BoxFit.fill,
+						),
+					),
+
+					Column(
+						mainAxisAlignment: MainAxisAlignment.center,
+						crossAxisAlignment: CrossAxisAlignment.center,
+						children: <Widget>[
+
+							Center(
+								child: Text(
+									"Travelap",
+									style: TextStyle(
+										color: Colors.white,
+										fontWeight: FontWeight.bold,
+										fontSize: 40.0,
+									),
+								),
+							),
+
+							Center(
+								child: Text(
+									"solo travel",
+									style: TextStyle(
+										color: Colors.white,
+										fontSize: 20.0,
+										fontWeight: FontWeight.bold,
+									),
+								),
+							),
+
+						],
+					),
+				],
 			),
 		);
  	}
